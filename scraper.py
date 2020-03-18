a5fb1cf1e45e69ada7f3c379b7d61810363f93e0
Bartosz Laskowsk <laskowski.bartosz99@gmail.com>
2020-03-18 15:01:24 +0100

    BL 2020.03.18

--- scraper.py
+++ scraper.py
@@ -13,16 +13,42 @@ page_tree = BeautifulSoup(page_respons.text, "html.parser")
 opinions = page_tree.find_all("li", "review-box")
 
 #wydobycie składowych dla pojedynczej opinii
-opinion = opinions.pop()
+for opinion in opinions:
 
-opinion_id = opinion["data-entry-id"]
-author = opinion.find("div", "reviewer-name-line").string
-recomendation = opinion.find("div", "product-review-summary").find("em").string
-stars = opinion.find("span","review-score-count").string
-purchased = opinion.find("div", "product-review-pz").string
+    opinion_id = opinion["data-entry-id"]
+    author = opinion.find("div", "reviewer-name-line").string
+    recomendation = opinion.find("div", "product-review-summary").find("em").string
+    stars = opinion.find("span","review-score-count").string
+    try:
+        purchased = opinion.find("div", "product-review-pz").string
+    except IndexError:
+        purchased = None
+    dates = opinion.finf("span", "review-time").find_all("time").
+    review_date = dates.pop()["datetime"]
+    try:
+        purchase_date = dates.pop()["datetime"]
+    except IndexError:
+        purchase_date = None
 
-usefull = opinion.find("button", "vote-yes").find("span").string
-useless = opinion.find("button", "vote-no").find("span").string
-content = opinion.find("p", "product-review-body").get_text()
-print(opinion_id, recomendation, usefull, useless, content, stars)
+
+    usefull = opinion.find("button", "vote-yes").find("span").string
+    useless = opinion.find("button", "vote-no").find("span").string
+    content = opinion.find("p", "product-review-body").get_text()
+
+    try:
+        pros = opinion.find("div", "pros-cell").find("ul").get_text()
+    except IndexError:
+        pros = None
+
+    try:
+        cons = opinion.find("div", "pros-cell").find("ul").get_text()
+    except IndexError:
+        cons = None
+
+
+## Etap 2 - pobieranie skłądowych wszystkich opinii z pojedynczej strony
+-zapisanie składowych opinii w złożonej strukturze danych
+## Etap 3 
+- przechodenie po stronach z opiniami
+- eksport opinii do pliku (.csv lub .xlsx lub .json)
 
